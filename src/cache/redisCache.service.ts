c7e10b16734450fae69f5b4e6e915579ba3d33ce
AtofStryker <bglesias@gmail.com>
2021-10-06 11:48:08 -0400

    docs(redis thread safety): add some docs explaining redis implementation
    This is to shed light/clarity on implementation choices

--- src/cache/redisCache.service.ts
+++ src/cache/redisCache.service.ts
@@ -1,6 +1,13 @@
 import { Injectable, Inject, CACHE_MANAGER } from '@nestjs/common';
 import { Cache } from 'cache-manager';
 
+/**
+ * Since redis isn't thread safe by itself, something like redis-lock can be used
+ * in tandem with redis to make sure resources are leased out without repercussions
+ *
+ * For our implementation, this should provide basic thread safety for a single call, but multiple spammed calls
+ * to the API could result in some non thread safe operations
+ */
 @Injectable()
 export class RedisCacheService {
   constructor(@Inject(CACHE_MANAGER) private readonly cache: Cache) {}
